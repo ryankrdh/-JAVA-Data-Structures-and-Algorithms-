926958f5f20781a1fd243eaf764b809663d2e19e
ryankrdh <ryankrdh@gmail.com>
2022-10-10 15:43:35 -0700

    notes

---
+++ JavaTutorial/src/com/javanotes/Main.java
@@ -0,0 +1,16 @@
+package com.javanotes;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String message = "Hello Wor  ld   ";
+//        System.out.println(message.replace(" ", ""));
+        System.out.println(message.trim());
+
+//        Point point1 = new Point(1, 1);
+//        Point point2 = point1;
+//        System.out.println(point2);
+
+    }
+}
