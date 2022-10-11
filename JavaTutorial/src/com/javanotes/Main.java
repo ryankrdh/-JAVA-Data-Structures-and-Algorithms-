06132f50fea2c75bd6a38bc8bc343a690672d306
ryankrdh <ryankrdh@gmail.com>
2022-10-11 10:56:13 -0700

    notes

--- JavaTutorial/src/com/javanotes/Main.java
+++ JavaTutorial/src/com/javanotes/Main.java
@@ -1,18 +1,28 @@
 package com.javanotes;
 
 import java.awt.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello Wor  ld   ";
+//        String message = "Hello Wor  ld   ";
 //        System.out.println(message.replace(" ", ""));
-        System.out.println(message.trim());
+//        System.out.println(message.trim());
 
 //        Point point1 = new Point(1, 1);
 //        Point point2 = point1;
 //        System.out.println(point2);
+//        int[] numbers = { 2, 3, 4, 1, 5 };
+//        System.out.println(numbers.length);
+//        System.out.println(Arrays.toString(numbers));
+//
+//        String str1 = "anagram";
+//        int[] numbers = new int[];
+ //        String[] arr1 = {str1};
+//        Arrays.sort(arr1);
+//        System.out.println(Arrays.toString(arr1));
 
+        int result = (int)Math.ceil(1.1F);
+        System.out.println(result);
     }
 }
-
-//this is a test
