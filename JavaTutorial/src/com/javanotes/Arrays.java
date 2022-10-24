c15ab3390478d35c4ba92e4f1ae5e365cc4f178c
ryankrdh <ryankrdh@gmail.com>
2022-10-24 10:01:55 -0700

    leetcode notes

--- JavaTutorial/src/com/javanotes/Arrays.java
+++ JavaTutorial/src/com/javanotes/Arrays.java
@@ -468,3 +468,28 @@ class Solution {
     }
 }
 // O(n)
+
+
+// 153. Find Minimum in Rotated Sorted Array
+class Solution {
+    public int findMin(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        // Binary search with two pointers
+        int left = 0;
+        int right = nums.length - 1;
+
+        while left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
+// same as binary search O(logN)
