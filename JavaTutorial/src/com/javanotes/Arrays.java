e7a3b765867a11ca74881dec14c3869cdd240978
ryankrdh <ryankrdh@gmail.com>
2022-10-24 11:09:05 -0700

    leetcode notes

--- JavaTutorial/src/com/javanotes/Arrays.java
+++ JavaTutorial/src/com/javanotes/Arrays.java
@@ -470,7 +470,8 @@ class Solution {
 // O(n)
 
 
-// 153. Find Minimum in Rotated Sorted Array
+// 153. Find Minimum in Rotated Sorted Array.
+// must solve in O(log n)
 class Solution {
     public int findMin(int[] nums) {
         if (nums.length == 1) {
@@ -493,3 +494,18 @@ class Solution {
     }
 }
 // same as binary search O(logN)
+
+
+// 1480 Running sum of 1d Array.
+class Solution {
+    public int[] runningSum(int[] nums) {
+
+        // nums[1] since nums[0] stays the same.
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i-1];
+        }
+
+        return nums;
+    }
+}
+// O(n)
