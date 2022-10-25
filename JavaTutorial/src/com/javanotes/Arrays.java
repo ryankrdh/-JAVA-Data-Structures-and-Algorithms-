8833fe3cdccc908248474c8fa5042454677da3a7
ryankrdh <ryankrdh@gmail.com>
2022-10-24 17:24:11 -0700

    leetcode notes

--- JavaTutorial/src/com/javanotes/Arrays.java
+++ JavaTutorial/src/com/javanotes/Arrays.java
@@ -509,3 +509,37 @@ class Solution {
     }
 }
 // O(n)
+
+
+// 33. Search in Rotated Sorted Array
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[left] <= nums[mid]) {
+                // if the target is in the left half, move the right pointer to the left.
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    // Then move the left pointer to the right.
+                    left = mid + 1;
+                }
+            } else {
+                // if the target is in the right half, move the left pointer to the right.
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    // Then move the right pointer to the left
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+// O(logN)
