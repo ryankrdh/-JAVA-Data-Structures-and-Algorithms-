f4e0dd0de739ab8e14c9f1370c710e07b69f6138
ryankrdh <ryankrdh@gmail.com>
2022-10-25 20:58:03 -0700

    leetcode notes

--- JavaTutorial/src/com/javanotes/Arrays.java
+++ JavaTutorial/src/com/javanotes/Arrays.java
@@ -482,7 +482,7 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
 
-        while left < right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] > nums[right]) {
                 left = mid + 1;
