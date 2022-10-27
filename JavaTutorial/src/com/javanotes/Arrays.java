943a288c9e6b02b439b9277bd5ff3b14bbf4522d
ryankrdh <ryankrdh@gmail.com>
2022-10-26 22:51:14 -0700

    leetcode notes

--- JavaTutorial/src/com/javanotes/Arrays.java
+++ JavaTutorial/src/com/javanotes/Arrays.java
@@ -449,6 +449,21 @@ class Solution {
 // O(n)
 
 
+//122. Best Time to Buy and Sell Stock II
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxprofit += prices[i] - prices[i-1];
+            }
+        }
+        return maxprofit;
+    }
+}
+//O(n)
+
+
 // 217. Contains Duplicate
 class Solution {
     public boolean containsDuplicate(int[] nums) {
@@ -639,3 +654,6 @@ class Solution {
     }
 }
 // O(n)
+
+
+//
