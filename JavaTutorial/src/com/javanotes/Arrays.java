52d24a987e7e9b26a6895c64b12a8272ac7a47a6
ryankrdh <ryankrdh@gmail.com>
2022-11-05 21:07:57 -0700

    leetcode notes

--- JavaTutorial/src/com/javanotes/Arrays.java
+++ JavaTutorial/src/com/javanotes/Arrays.java
@@ -1004,3 +1004,248 @@ class Solution {
         // time O(N). space O(1)
     }
 }
+
+
+// SINGLY LINKED LIST SYNTAX NOTES
+public class SinglyLinkedList<T> {
+    //Node inner class for SLL
+    public class Node {
+        public T data;
+        public Node nextNode;
+
+    }
+
+    //head node of the linked list
+    public Node headNode;
+    public int size;
+
+    //constructor
+    public SinglyLinkedList() {
+        headNode = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+
+        if (headNode == null) return true;
+        return false;
+    }
+
+    //Inserts new data at the start of the linked list
+    public void insertAtHead(T data) {
+        //Creating a new node and assigning it the new data value
+        Node newNode = new Node();
+        newNode.data = data;
+        //Linking head to the newNode's nextNode
+        newNode.nextNode = headNode;
+        headNode = newNode;
+        size++;
+    }
+
+    //Inserts new data at the end of the linked list
+    public void insertAtEnd(T data) {
+        //if the list is empty then call insertATHead()
+        if (isEmpty()) {
+            insertAtHead(data);
+            return;
+        }
+        //Creating a new Node with value data
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.nextNode = null;
+
+        Node last = headNode;
+        //iterate to the last element
+        while (last.nextNode != null) {
+            last = last.nextNode;
+        }
+        //make newNode the next element of the last node
+        last.nextNode = newNode;
+        size++;
+    }
+
+    //inserts data after the given prev data node
+    public void insertAfter(T data, T previous) {
+
+        //Creating a new Node with value data
+        Node newNode = new Node();
+        newNode.data = data;
+        //Start from head node
+        Node currentNode = this.headNode;
+        //traverse the list until node having data equal to previous is found
+        while (currentNode != null && currentNode.data != previous) {
+            currentNode = currentNode.nextNode;
+        }
+        //if such a node was found
+        //then point our newNode to currentNode's nextElement
+        if (currentNode != null) {
+            newNode.nextNode = currentNode.nextNode;
+            currentNode.nextNode = newNode;
+            size++;
+        }
+    }
+
+    public void printList() {
+        if (isEmpty()) {
+            System.out.println("List is Empty!");
+            return;
+        }
+
+        Node temp = headNode;
+        System.out.print("List : ");
+
+        while (temp.nextNode != null) {
+            System.out.print(temp.data.toString() + " -> ");
+            temp = temp.nextNode;
+        }
+
+        System.out.println(temp.data.toString() + " -> null");
+    }
+
+    //Searches a value in the given list.
+    public boolean searchNode(T data) {
+        //Start from first element
+        Node currentNode = this.headNode;
+
+        //Traverse the list till you reach end
+        while (currentNode != null) {
+            if (currentNode.data.equals(data))
+                return true; //value found
+
+            currentNode = currentNode.nextNode;
+        }
+        return false; //value not found
+    }
+
+    //Deletes data from the head of list
+    public void deleteAtHead() {
+        //if list is empty then simply return
+        if (isEmpty())
+            return;
+        //make the nextNode of the headNode equal to new headNode
+        headNode = headNode.nextNode;
+        size--;
+    }
+
+    //Deletes data given from the linked list
+    public void deleteByValue(T data) {
+        //if empty then simply return
+        if (isEmpty())
+            return;
+
+        //Start from head node
+        Node currentNode = this.headNode;
+        Node prevNode = null; //previous node starts from null
+
+        if(currentNode.data.equals(data)) {
+            //data is at head so delete from head
+            deleteAtHead();
+            return;
+        }
+        //traverse the list searching for the data to delete
+        while (currentNode != null) {
+            //node to delete is found
+            if (data.equals(currentNode.data)){
+                prevNode.nextNode = currentNode.nextNode;
+                size--;
+                return;
+            }
+            prevNode = currentNode;
+            currentNode = currentNode.nextNode;
+        }
+    }
+}
+
+
+// DOUBLE LINKED LIST SYNTAX NOTES
+public class DoublyLinkedList<T> {
+
+    //Node inner class for DLL
+    public class Node {
+        public T data;
+        public Node nextNode;
+        public Node prevNode;
+    }
+
+    public Node headNode;
+    public int size;
+
+    public DoublyLinkedList() {
+        this.headNode = null;
+    }
+
+    //checks if the list is empty
+    public boolean isEmpty() {
+        if (headNode == null)
+            return true; //is empty
+        return false;    //is not empty
+    }
+
+    public void insertAtHead(T data) {
+        //create node and put in the data
+        Node newNode = new Node();
+        newNode.data = data;
+        // Make next of new node as head and previous as NULL
+        newNode.nextNode = this.headNode;
+        newNode.prevNode = null;
+        //Change previous of head node to new node
+        if (headNode != null)
+            headNode.prevNode = newNode;
+        this.headNode = newNode;
+        size++;
+    }
+
+    public void printList() {
+        if (isEmpty()) {
+            System.out.println("List is Empty!");
+            return;
+        }
+
+        Node temp = headNode;
+        System.out.print("List : null <- ");
+
+        while (temp.nextNode != null) {
+            System.out.print(temp.data.toString() + " <-> ");
+            temp = temp.nextNode;
+        }
+
+        System.out.println(temp.data.toString() + " -> null");
+    }
+    //deletes the first element
+    public void deleteAtHead(){
+        //if list is empty do nothing
+        if(isEmpty())
+            return;
+
+        //if List is not empty then link head to the
+        //nextElement of firstElement.
+        headNode = headNode.nextNode;
+        headNode.prevNode = null;
+        size--;
+    }
+    public void deleteByValue(T data) {
+        //if empty then simply return
+        if (isEmpty())
+            return;
+
+        //Start from head node
+        Node currentNode = this.headNode;
+
+        if (currentNode.data.equals(data)) {
+            //data is at head so delete from head
+            deleteAtHead();
+            return;
+        }
+        //traverse the list searching for the data to delete
+        while (currentNode != null) {
+            //node to delete is found
+            if (data.equals(currentNode.data)) {
+                currentNode.prevNode.nextNode = currentNode.nextNode;
+                if(currentNode.nextNode != null)
+                    currentNode.nextNode.prevNode = currentNode.prevNode;
+                size--;
+            }
+            currentNode = currentNode.nextNode;
+        }
+    }
+}
