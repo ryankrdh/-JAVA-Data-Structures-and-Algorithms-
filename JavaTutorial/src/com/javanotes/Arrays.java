2078c01cc295b9d9004e3e3d91955417e4862738
ryankrdh <ryankrdh@gmail.com>
2022-11-09 23:13:33 -0800

    leetcode notes

--- JavaTutorial/src/com/javanotes/Arrays.java
+++ JavaTutorial/src/com/javanotes/Arrays.java
@@ -247,7 +247,7 @@ class CheckSecondMax {
 
         for(int i = 0; i < arr.length; i++){
             if(arr[i] > max){
-                //so that we dod not lose max
+                //so that we do not lose max
                 secondMax = max;
                 max = arr[i];
             }else if(arr[i] > secondMax && arr[i] != max){
