a86ba460a4f9a7e4a8106342569d09baca78678e
ryankrdh <ryankrdh@gmail.com>
2022-11-28 21:29:42 -0800

    leetcode notes

--- JavaTutorial/src/com/javanotes/Arrays.java
+++ JavaTutorial/src/com/javanotes/Arrays.java
@@ -2419,3 +2419,52 @@ Input
 // Time O(1). Space O(N).
 
 
+// 225. Implement Stack using Queues
+class MyStack {
+
+    Queue<Integer> queue;
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        queue.add(x);
+        // reverse the elements in queue
+        for(int i = 0; i < queue.size() - 1; i++){ // queue.size() - 1 in order to move the recently added element to the first in line.
+            queue.add(queue.poll());
+        }
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.poll();
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return queue.peek();
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+
+// Everything is O(1) time other than push is O(N) time.
+// for push(); we need to reverse the elements in the queue (FIFO) since stack is FILO. queue.add(queue.poll());
+// after we reverse, now the order in the queue is the same as stack.
+
+
