9d1926b24ad61a1513bbdee598b9ed513d142a79
ryankrdh <ryankrdh@gmail.com>
2022-11-28 20:36:36 -0800

    leetcode notes

--- JavaTutorial/src/com/javanotes/Arrays.java
+++ JavaTutorial/src/com/javanotes/Arrays.java
@@ -2381,3 +2381,40 @@ class Solution {
     }
 }
 // time O(N). space O(1)
+
+
+
+// 346. Moving Average from Data Stream.
+class MovingAverage {
+    // Circular Queue
+    /*
+Input
+["MovingAverage", "next", "next", "next", "next"]
+[[3], [1], [10], [3], [5]]
+    */
+
+    Queue<Integer> q = new LinkedList();
+    double sum = 0; // in case of overflow
+    int size;
+
+    public MovingAverage(int s) { // Calls the MovingAverage located at [0]
+        size = s;
+    }
+
+    public double next(int val) { // Calls the next()
+        if(q.size() == size){
+            sum = sum - q.poll(); // removes the first element of the Queue. peek() retrieves the element but does not remove. Both returns NULL if the queue is empty
+        }
+        q.offer(val); // same as add() but does not throw an exception when the capacity is full
+        sum += val;
+        return sum/q.size();
+    }
+}
+/*
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
+// Time O(1). Space O(N).
+
+
